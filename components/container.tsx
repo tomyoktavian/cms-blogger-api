338b9bd340625dd87b478761df0351142ac1625f
tomyoktavian <drivehartomy@gmail.com>
2022-08-28 18:21:28 +0700

    first commit

---
+++ components/container.tsx
@@ -0,0 +1,15 @@
+import { cx } from "@utils/classnames";
+
+export default function Container(props: any) {
+  return (
+    <div
+      className= {
+      cx(
+        "container px-8 py-5 lg:py-8 mx-auto xl:px-5 max-w-screen-lg",
+        props.className
+      )
+    }>
+    { props.children }
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Container from '@components/container'
 
 const Home: NextPage = () => {
   return (
@@ -13,44 +14,95 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <Container><div className="bg-white">
+          <div className="container flex flex-col justify-center px-6 py-6 mx-auto sm:py-12">
+            <div className="grid gap-10 lg:grid-cols-2">
+              <div className="w-full overflow-hidden rounded-sm aspect-w-16 aspect-h-9">
+                <img className="object-cover" src="https://images.unsplash.com/photo-1630702379394-e202e2fbe01e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=600&amp;q=80" alt="image" />
+              </div>
+              <div className="flex flex-col justify-center">
+                <div className="flex space-x-3">
+                  <span className="px-3 py-1 text-sm rounded-full bg-emerald-100 text-emerald-600">Engineering</span>
+                  <span className="px-3 py-1 text-sm rounded-full bg-amber-100 text-amber-600">Design</span>
+                </div>
+                <h3 className="mt-5 text-2xl font-bold">20 Myths About TailwindCSS</h3>
+                <p className="mt-3 text-gray-500">Equity agile development burn rate buyer strategy founders user experience customer. Termsheet series A financing direct mailing infrastructure research &amp; development twitter.</p>
+                <div className="flex mt-5 space-x-3">
+                  <div className="flex-shrink-0">
+                    <img className="object-cover w-12 h-12 rounded-full" src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=100&amp;q=80" alt="image" />
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="font-medium">Ronald Matthews</span>
+                    <span className="text-gray-500">Oct 14, 2021 • 6 min read</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <h2 className="mt-10 text-3xl">Latest Articles</h2>
+            <div className="grid mt-5 lg:grid-cols-3 gap-y-14 lg:gap-10">
+              <div>
+                <div className="w-full overflow-hidden rounded-sm aspect-w-13 aspect-h-9">
+                  <img className="object-cover" src="https://images.unsplash.com/photo-1630702379394-e202e2fbe01e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=600&amp;q=80" alt="image" />
+                </div>
+                <div className="flex mt-5 space-x-3">
+                  <span className="px-3 py-1 text-sm rounded-full bg-emerald-100 text-emerald-600">Engineering</span>
+                  <span className="px-3 py-1 text-sm rounded-full bg-amber-100 text-amber-600">Design</span>
+                </div>
+                <h3 className="mt-5 text-2xl font-bold">20 Myths About TailwindCSS</h3>
+                <p className="mt-3 text-gray-500">Equity agile development burn rate buyer strategy founders user experience customer. Termsheet series A financing direct mailing infrastructure research &amp; development twitter.</p>
+                <div className="flex mt-5 space-x-3">
+                  <div className="flex-shrink-0">
+                    <img className="object-cover w-12 h-12 rounded-full" src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=100&amp;q=80" alt="image" />
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="font-medium">Ronald Matthews</span>
+                    <span className="text-gray-500">Oct 14, 2021 • 6 min read</span>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="w-full overflow-hidden rounded-sm aspect-w-13 aspect-h-9">
+                  <img className="object-cover" src="https://images.unsplash.com/photo-1601933470096-0e34634ffcde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80" alt="image" />
+                </div>
+                <div className="flex mt-5 space-x-3">
+                  <span className="px-3 py-1 text-sm rounded-full bg-fuchsia-100 text-fuchsia-600">Product</span>
+                  <span className="px-3 py-1 text-sm rounded-full bg-emerald-100 text-emerald-600">Engineering</span>
+                </div>
+                <h3 className="mt-5 text-2xl font-bold">15 Best TailwindCSS Website Templates</h3>
+                <p className="mt-3 text-gray-500">Agile development burn rate buyer strategy founders user experience customer. Termsheet series A financing direct mailing infrastructure research &amp; development twitter.</p>
+                <div className="flex mt-5 space-x-3">
+                  <div className="flex-shrink-0">
+                    <img className="object-cover w-12 h-12 rounded-full" src="https://images.unsplash.com/photo-1541647376583-8934aaf3448a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=100&amp;q=80" alt="image" />
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="font-medium">Joseph Peck</span>
+                    <span className="text-gray-500">Sep 03, 2021 • 6 min read</span>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="w-full overflow-hidden rounded-sm aspect-w-13 aspect-h-9">
+                  <img className="object-cover" src="https://images.unsplash.com/photo-1630283018262-d0df4afc2fef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80" alt="image" />
+                </div>
+                <div className="flex mt-5 space-x-3">
+                  <span className="px-3 py-1 text-sm rounded-full bg-amber-100 text-amber-600">Design</span>
+                </div>
+                <h3 className="mt-5 text-2xl font-bold">Why Top brands switch to TailwindCSS</h3>
+                <p className="mt-3 text-gray-500">Development burn rate buyer strategy founders user experience customer. Termsheet series A financing direct mailing infrastructure research &amp; development twitter.</p>
+                <div className="flex mt-5 space-x-3">
+                  <div className="flex-shrink-0">
+                    <img className="object-cover w-12 h-12 rounded-full" src="https://images.unsplash.com/photo-1549351512-c5e12b11e283?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=100&amp;q=80" alt="image" />
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="font-medium">Emily Gilbert</span>
+                    <span className="text-gray-500">Aug 20, 2021 • 6 min read</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        </Container>
       </main>
 
       <footer className={styles.footer}>
